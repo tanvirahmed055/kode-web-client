3cb580deed17b8db05b538758f9d7a7c28d0bd6c
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-12-16 19:13:26 +0600

    added Technology component

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Header from '../../Shared/Header/Header';
 import Footer from '../../Shared/Footer/Footer';
+import Technology from '../Technology/Technology';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Technology></Technology>
             <Footer></Footer>
         </div>
     );
---
+++ src/Pages/Home/Technology/Technology.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import { Card, Col, Image, Row } from 'react-bootstrap';
+import './Technology.css';
+
+
+const Technology = () => {
+
+    return (
+        <>
+
+            <div className="container-fluid mt-2 mb-5 pt-5" id="features">
+                <h1 className="fw-bolder mb-3 text-primary">We love technology </h1>
+                <p className="text-start pb-3 pt-3 text-secondary ps-5 pe-5">We love to work building software along with the new challenges that come on the way. We are always in our innovative mode which ever project we do - we are always driven by passion, creativity and innovation. Our expert skills and experience along with our proven development method makes your dream a reality.</p>
+
+                <Row xs={1} md={6} className="g-4 d-flex align-items-center mt-4">
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/html-5--v1.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> HTML5</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/css3.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> CSS3</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/javascript--v1.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> JavaScript</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/bootstrap.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Bootstrap 5</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/officel/48/000000/react.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> React.js</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/html-5--v1.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Node.js</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://www.vectorlogo.zone/logos/expressjs/expressjs-ar21.svg" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Express.js</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/firebase.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Firebase Authentication</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/java-web-token.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> JSON Web Token</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/mongodb.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> MongoDB Atlas</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/npm.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> NPM</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-yarn-fast-reliable-and-secure-dependency-management-web-portal-logo-shadow-tal-revivo.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Yarn</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/ios-glyphs/48/000000/github.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Git</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-color-tal-revivo.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Netlify</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/heroku.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold"> Heroku</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/adobe-xd--v1.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold">Adobe XD</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/figma.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold">Figma</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card className="text-center  shadow-lg p-1 mb-5 bg-body rounded card">
+                            <span className="icon-color pt-2">
+                                <Image src="https://img.icons8.com/color/48/000000/webflow.png" fluid />
+                            </span>
+                            <Card.Body>
+                                <Card.Title className="fw-bold">Webflow</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+
+                </Row>
+            </div>
+
+        </>
+
+    );
+};
+
+export default Technology;
+
