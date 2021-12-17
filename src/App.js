067ced1e7894854d641de4f2994a1bd95aa8dbe2
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-12-17 21:32:27 +0600

    created Register component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PurchaseService from './Pages/PurchaseService/PurchaseService';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/purchaseService/:id" element={
             <PurchaseService />
           } >
---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
+import {
+    useNavigate
+} from "react-router-dom";
+import useFirebase from '../../../hooks/useFirebase';
+
+const Register = () => {
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+
+    const { handleRegistration, updateProfile, auth } = useFirebase();
+
+    let navigate = useNavigate();
+
+
+
+
+    const onSubmit = (data, e) => {
+        //console.log(data);
+
+        // const newUser = {
+        //     name: data?.name,
+        //     email: data?.email,
+        //     password: data?.password
+        // }
+
+        //console.log(newUser);
+
+
+        handleRegistration(data.name, data.email, data.password)
+            .then(result => {
+                // Signed in 
+                //const user = result.user;
+                // ...
+                updateProfile(auth.currentUser, {
+                    displayName: data.name,
+                }).then(() => {
+                    // Profile updated!
+                    // ...
+                }).catch((error) => {
+                    // An error occurred
+                    // ...
+                });
+
+
+                reset();
+                navigate('/login')
+
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+                console.log(errorCode, errorMessage);
+            });
+
+
+
+    }
+
+
+
+
+
+    return (
+        <Container fluid >
+            <Row>
+                <Col sm={12} >
+                    <Container className="order-form-container py-3  my-5 ">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <h1 className="fw-bold">Sign Up</h1>
+
+                            <label htmlFor="name">Name</label>
+                            <input placeholder="name" type="text" {...register("name")} />
+
+
+                            <label htmlFor="email">Email</label>
+                            <input placeholder="email" type="email" {...register("email")} />
+
+
+                            <label htmlFor="password">Password</label>
+                            <input placeholder="password" type="password" {...register("password")} />
+
+
+                            <div style={{ color: "red" }}>
+                                {Object.keys(errors).length > 0 &&
+                                    "There are errors, check your console."}
+                            </div>
+                            <input type="submit" />
+                        </form>
+                    </Container>
+
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Register;
